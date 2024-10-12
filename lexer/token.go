da29539a4b822da53107136589978cefba89a25c
Vishruth Devan <vishruthdevan@gmail.com>
2024-10-12 10:48:36 -0400

    feat: define types of tokens

---
+++ lexer/token.go
@@ -0,0 +1,36 @@
+package lexer
+
+type TokenType string
+
+type Token struct {
+	Type  TokenType
+	Value string
+}
+
+const (
+	KEYWORD = "KEYWORD"
+	INDENTIFIER = "INDENTIFIER"
+	INTEGER     = "INTEGER"
+	FLOAT       = "FLOAT"
+	STRING      = "STRING"
+	
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	
+	COMMA     = ","
+	SEMICOLON = ";"
+	
+	LPR      = "("
+	RPR      = ")"
+	LBRACE   = "{"
+	RBRACE   = "}"
+	LBRACKET = "["
+	RBRACKET = "]"
+	
+	EOF     = "EOF"
+	ILLEGAL = "ILLEGAL"
+)
