6de6aafae54d4ba807b8cb4f40e4bf11b10d0f4a
Vishruth Devan <vishruthdevan@gmail.com>
2024-10-12 11:37:29 -0400

    feat: add keywords to list of tokens

--- lexer/token.go
+++ lexer/token.go
@@ -8,28 +8,54 @@ type Token struct {
 }
 
 const (
-	KEYWORD = "KEYWORD"
+	KEYWORD     = "KEYWORD"
 	INDENTIFIER = "INDENTIFIER"
 	INTEGER     = "INTEGER"
 	FLOAT       = "FLOAT"
 	STRING      = "STRING"
-	
+
 	ASSIGN   = "="
 	PLUS     = "+"
 	MINUS    = "-"
 	ASTERISK = "*"
 	SLASH    = "/"
-	
+
 	COMMA     = ","
 	SEMICOLON = ";"
-	
+
 	LPR      = "("
 	RPR      = ")"
 	LBRACE   = "{"
 	RBRACE   = "}"
 	LBRACKET = "["
 	RBRACKET = "]"
-	
+
 	EOF     = "EOF"
 	ILLEGAL = "ILLEGAL"
+
+	FUNCTION = "FUNCTION"
+	RETURN   = "RETURN"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	NULL     = "NULL"
+	FOR      = "FOR"
+	IN       = "IN"
+	LOAD     = "LOAD"
+	EXPORT   = "EXPORT"
 )
+
+var keywords = map[string]TokenType{
+	"function":      FUNCTION,
+	"return":   RETURN,
+	"if":       IF,
+	"else":     ELSE,
+	"true":     TRUE,
+	"false":    FALSE,
+	"null":     NULL,
+	"for":      FOR,
+	"in":       IN,
+	"load":     LOAD,
+	"export":   EXPORT,
+}
