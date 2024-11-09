2dfc82c5d95dfc9ed668bcffad41b32bf0ebf697
Vishruth Devan <vishruthdevan@gmail.com>
2024-11-09 17:59:45 -0500

    chore: make sample files unique to each module

--- test-lexer.sh
+++ test-lexer.sh
@@ -1,8 +1,8 @@
 if [ $# -eq 0 ]; then
-    echo "Usage: ./test-lexer.sh /wavy/<path-to-sample-file.vy>"
+    echo "Usage: ./test-lexer.sh /wavy/lexer/samples/<sample-file.vy>"
     exit 1
 fi
 
 docker build --rm -t lexer-test .
-docker run -v "./samples:/wavy/samples" --rm lexer-test go test -v /wavy/lexer/... -args -file=$1
+docker run -v "./lexer/samples:/wavy/lexer/samples" --rm lexer-test go test -v /wavy/lexer/... -args -file=$1
 docker rmi lexer-test
