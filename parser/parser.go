7bb599701a5231b89a912ad86db72bc28acfc62d
Vishruth Devan <vishruthdevan@gmail.com>
2024-11-11 12:24:49 -0500

    feat: create data structures to implement pratt parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,14 +1,61 @@
 package parser
 
 import (
+	"fmt"
 	"wavy/lexer"
 )
 
 type Parser struct {
-	lexer        *lexer.Lexer
-	currentToken lexer.Token
-	peekToken    lexer.Token
-	errors       []string
+	lexer          *lexer.Lexer
+	currentToken   lexer.Token
+	peekToken      lexer.Token
+	errors         []string
+	prefixParseFns map[lexer.TokenType]prefixParseFn
+	infixParseFns  map[lexer.TokenType]infixParseFn
+}
+
+type (
+	prefixParseFn func() Expression
+	infixParseFn  func(Expression) Expression
+)
+
+func (p *Parser) registerPrefix(tokenType lexer.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+func (p *Parser) registerInfix(tokenType lexer.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
+}
+
+func (parser *Parser) Errors() []string {
+	return parser.errors
+}
+
+func (parser *Parser) peekError(t lexer.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, parser.peekToken.Type)
+	parser.errors = append(parser.errors, msg)
+}
+
+func (parser *Parser) nextToken() {
+	parser.currentToken = parser.peekToken
+	parser.peekToken = parser.lexer.NextToken()
+}
+
+func (p *Parser) isCurrentToken(t lexer.TokenType) bool {
+	return p.currentToken.Type == t
+}
+
+func (p *Parser) isPeekToken(t lexer.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) peek(t lexer.TokenType) bool {
+	if p.isPeekToken(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
 }
 
 func Init(lexer *lexer.Lexer) *Parser {
