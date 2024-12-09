8c151d164e02f5a91a3cc37ab139a8f4aea5b9e0
Vishruth Devan <vishruthdevan@gmail.com>
2024-12-09 11:21:01 -0800

    feat: evaluate add, sub, mul and div

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -59,9 +59,16 @@ func (c *Compiler) Compile(node parser.Node) error {
 		switch node.Operator {
 		case "+":
 			c.emit(code.OpAdd)
+		case "-":
+			c.emit(code.OpSub)
+		case "*":
+			c.emit(code.OpMul)
+		case "/":
+			c.emit(code.OpDiv)
 		default:
 			return fmt.Errorf("unknown operator %s", node.Operator)
 		}
+
 	case *parser.IntegerValue:
 		integer := &object.Integer{Value: node.Value}
 		c.emit(code.OpConstant, c.addConstant(integer))
