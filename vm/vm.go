52cc6d9c1eb19c7ce86b996aa6d1c75567ba1254
Vishruth Devan <vishruthdevan@gmail.com>
2024-12-09 10:25:25 -0800

    feat: push bytecode into stack

--- vm/vm.go
+++ vm/vm.go
@@ -1,6 +1,7 @@
 package vm
 
 import (
+	"fmt"
 	"wavy/code"
 	"wavy/compiler"
 	"wavy/object"
@@ -22,4 +23,37 @@ func Init(bytecode *compiler.Bytecode) *VM {
 		stack:        make([]object.Object, StackSize),
 		sp:           0,
 	}
+}
+
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+	return vm.stack[vm.sp-1]
+}
+
+func (vm *VM) Run() error {
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := code.Opcode(vm.instructions[ip])
+		switch op {
+		case code.OpConstant:
+			constIndex := code.ReadUint16(vm.instructions[ip+1:])
+			ip += 2
+
+			err := vm.push(vm.constants[constIndex])
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (vm *VM) push(o object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("stack overflow")
+	}
+	vm.stack[vm.sp] = o
+	vm.sp++
+	return nil
 }
